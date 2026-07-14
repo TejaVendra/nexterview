52c50f3aa5b9c916fb8313310914f3444bab53a8
Teja <www.vendrateja2006@gmail.com>
2026-07-14 17:08:23 +0530

    navbar is updating...

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import logo from '../../assets/logo.png'
 import {motion} from 'motion/react'
 import { Link } from "react-router-dom";
-
+import { useSelector } from 'react-redux';
+import { CgProfile } from "react-icons/cg";
 const Navbar = () => {
+
+  const { user , authLoader } = useSelector((state) => state.auth)
   return (
 
     <nav className='w-[95%] mx-auto fixed top-3  left-0 right-0 z-50'>
@@ -22,7 +25,7 @@ const Navbar = () => {
             
             <Link href='/signup'
                      className=' font-semibold text-white bg-blue-700 rounded-full py-2 px-3 sm:py-3 sm:px-5 mr-3 sm:mr-5 shadow-inner cursor-pointer hover:bg-blue-800 transition duration-500'>
-                   Get Started
+                   {user ? <CgProfile/> : 'Get Started'}
             </Link> 
         </div>
     </nav>
